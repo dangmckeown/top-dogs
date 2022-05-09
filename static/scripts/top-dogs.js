f5bc1f451b3d94dfcfcbfc557417ebc55e33014c
Daniel McKeown <d.mckeown@mrushforthmac.lan>
2022-05-09 15:10:24 +0100

    Add example doggo to js

--- static/scripts/top-dogs.js
+++ static/scripts/top-dogs.js
@@ -1,18 +1,177 @@
-var dogs = {
-    afghan: {
+class Dog {
+    constructor(name, breed, weight, work_ethic, woofiness, winsomeness, wagginess, wisdom, bark, img)
+    {
+        this.name = name;
+        this.breed = breed;
+        this.weight = weight,
+        this.work_ethic = work_ethic;
+        this.woofiness = woofiness;
+        this.winsomeness = winsomeness;
+        this.wagginess = wagginess;
+        this.wisdom = wisdom;
+        this.bark = function(){
+            alert(bark);
+        }
+        this.img = img ? img : '/static/images/default-dog.png';
+    }
+}
+
+var exampleDog = new Dog(
+    'Craig', //name
+    'Siberian Husky', //breed
+    60, //weight
+    80, //work_ethic - super hard worker
+    65, //woofiness - bit excitable
+    35, // winsomeness - not everyone's cup of tea
+    40, //wagginess
+    30, //wisdom - bit mental
+    'Oaf!', //bark
+    '/static/images/siberian-husky-1.jpeg' //img
+    );
+
+//List of dog names to pick at random
+const dogNames = [
+    'Bella',
+'Luna',
+'Charlie',
+'Lucy',
+'Cooper',
+'Max',
+'Bailey',
+'Daisy',
+'Sadie',
+'Lola',
+'Buddy',
+'Molly',
+'Stella',
+'Tucker',
+'Bear',
+'Zoey',
+'Duke',
+'Harley',
+'Maggie',
+'Jax',
+'Bentley',
+'Milo',
+'Oliver',
+'Riley',
+'Rocky',
+'Penny',
+'Sophie',
+'Chloe',
+'Jack',
+'Lily',
+'Nala',
+'Piper',
+'Zeus',
+'Ellie',
+'Winston',
+'Toby',
+'Loki',
+'Murphy',
+'Roxy',
+'Coco',
+'Rosie',
+'Teddy',
+'Ruby',
+'Gracie',
+'Leo',
+'Finn',
+'Scout',
+'Dexter',
+'Ollie',
+'Koda',
+'Diesel',
+'Moose',
+'Mia',
+'Marley',
+'Gus',
+'Hank',
+'Willow',
+'Louie',
+'Thor',
+'Pepper',
+'Blue',
+'Jake',
+'Gunner',
+'Abby',
+'Apollo',
+'Bandit',
+'Kona',
+'Lilly',
+'Shadow',
+'Millie',
+'Nova',
+'Beau',
+'Buster',
+'Dixie',
+'Jackson',
+'Zoe',
+'Oakley',
+'Henry',
+'Ace',
+'Maverick',
+'Izzy',
+'Bruno',
+'Baxter',
+'Tank',
+'Layla',
+'Athena',
+'Ginger',
+'Olive',
+'Winnie',
+'Lexi',
+'Remi',
+'Jasper',
+'Hazel',
+'Gizmo',
+'Frankie',
+'Oscar',
+'Lucky',
+'Dakota',
+'Belle',
+'Brody'
+];
+
+
+/**
+ * Array breeds contains object for each breed
+ */
+var breeds = [
+    {
         name: "Afghan Hound",
         weight: {
-            lower: 50,
-            upper:60
+            min: 50,
+            max:60
         },
         work_ethic: {
-            lower: 0,
-            upper: 30
+            min: 0,
+            max: 30
         },
-        bark: function(){
-            alert('Awoooo');
-        }
-    }
+        winsomeness: {
+            //high ceiling, low floor!
+            min: 10,
+            max: 85
+        },
+        woofiness: {
+            //Can't be asked
+            min: 10,
+            max: 50
+        },
+        wagginess: {
+            //Too aloof to be very waggy
+            min: 5,
+            max: 45
+        },
+        wisdom: {
+            //a bit boh
+            min: 20,
+            max: 40
+        },
+        bark: 'Awoooo',
+        img: 'static/images/Afghan_dog_-_cropped.jpeg'
+        },
+];
     /*
     "Afghan Hound": {
         "weight": {
@@ -51,9 +210,14 @@ var dogs = {
         }
     }
     */
-};
 
+/**
+ * Generate Dog
+ * Pick random name from names list
+ * Pick breed at random from breeds list
+ */
 
-function testOne() {
-    console.log(dogs.afghan.work_ethic.upper);
+function generateDog(){
+    // Your code here
 }
+
